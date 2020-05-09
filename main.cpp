77684c062d7b2a46b849889f7c420893d52df520
Jens-Christian <beaumanvienna@users.noreply.github.com>
2020-05-08 19:04:25 -0700

    typo

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-// this is a test if the SDL2 renderer and bare opengl rendering work in the same program
+// this is a test to see if the SDL2 renderer and bare opengl rendering work in the same program
 
 // compile with
 // g++ main.cpp -o main -I/usr/include/SDL2 -I/usr/include/GL -lSDL2 -lGL -lSDL2_image
